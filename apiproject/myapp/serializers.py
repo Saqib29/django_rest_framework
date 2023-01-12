71bb5a9b43e953bc79947257f9fb474d2e5563a8
saqib <saqib29111@gmail.com>
2023-01-12 14:14:36 +0600

    rest api views model urls

--- apiproject/myapp/serializers.py
+++ apiproject/myapp/serializers.py
@@ -1,20 +1,25 @@
 from rest_framework import serializers
 from myapp.models import Contact
 
-class ContactSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=100)
-    title = serializers.CharField(max_length=100)
-    email = serializers.EmailField(max_length=30)
+# class ContactSerializer(serializers.Serializer):
+#     name = serializers.CharField(max_length=100)
+#     title = serializers.CharField(max_length=100)
+#     email = serializers.EmailField(max_length=30)
 
 
-    def create(self, validated_data):
-        return Contact.objects.create(validated_data)
+#     def create(self, validated_data):
+#         return Contact.objects.create(validated_data)
 
     
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.title = validated_data.get('title', instance.title)
-        instance.email = validated_data.get('email', instance.email)
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.email = validated_data.get('email', instance.email)
 
-        instance.save()
-        return instance
+#         instance.save()
+#         return instance
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = ['name', 'title', 'email']
