2990e4cfd37bdd61ec825ed7707920c14a29eae9
saqib <saqib29111@gmail.com>
2023-01-13 11:01:49 +0600

    Besic Authentication

--- apiproject/myapp/views.py
+++ apiproject/myapp/views.py
@@ -12,11 +12,16 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework import mixins
 from rest_framework import generics
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class ContactList(generics.ListCreateAPIView, mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
+
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
         return self.list(request)
